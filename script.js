858e4837b0b0fe45b2260fe204f001d0e50c6194
rupamghosal750-dotcom <rupamghosal750@gmail.com>
2026-06-27 22:42:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Website Loaded Successfully");
+const texts = [
+    "Accounting Professional",
+    "GST Professional",
+    "Income Tax Specialist",
+    "TDS Compliance",
+    "ROC Compliance"
+];
+
+let index = 0;
+
+const typing = document.getElementById("typing");
+
+function changeText(){
+    typing.textContent = texts[index];
+    index++;
+
+    if(index >= texts.length){
+        index = 0;
+    }
+}
+
+changeText();
+
+setInterval(changeText,2000);
