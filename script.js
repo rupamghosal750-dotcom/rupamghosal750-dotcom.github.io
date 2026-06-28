4b0d60f3dd305f7e33aff20bcd39cb470b11e940
rupamghosal750-dotcom <rupamghosal750@gmail.com>
2026-06-28 17:44:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,32 @@ function changeText(){
 changeText();
 
 setInterval(changeText,2000);
+// Scroll To Top
+
+const topBtn=document.getElementById("topBtn");
+
+window.onscroll=function(){
+
+if(document.body.scrollTop>300 || document.documentElement.scrollTop>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+};
+
+topBtn.onclick=function(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+};
